348289cc172bba06c91b684069d75c02db3f3a04
julioTiTE <jcaetite@hotmail.com>
2025-11-24 14:09:59 -0300

    Fix: desabilitar regra no-explicit-any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // ← ADICIONAR ESTA LINHA
+    },
+  },
 ];
 
 export default eslintConfig;
